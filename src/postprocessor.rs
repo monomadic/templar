65115366a3e5b50cff955e3227069b5b6cbbd968
nom <deathdisco@protonmail.com>
2020-04-13 00:05:14 +1000

    functional scopes

--- src/postprocessor.rs
+++ src/postprocessor.rs
@@ -7,32 +7,34 @@ pub fn run(nodes: Vec<Node>) -> ParseResult<Vec<UnwoundNode>> {
     let locals = extract_variables(&nodes);
     let fns = collect_function_definitions(&nodes)?;
 
-    let fns = HashMap::new(); // imported libs
-    let locals = HashMap::new(); // default globals (args?)
+    // let mut fns = HashMap::new(); // imported libs
+    // let mut locals = HashMap::new(); // default globals (args?)
 
-    unwind_children(&nodes, &locals, &fns)
+    unwind_children(&nodes, locals, fns)
 }
 
-fn unwind_children(nodes: &Vec<Node>, locals: &HashMap<String, Property>, fns: &HashMap<String, Function>) -> ParseResult<Vec<UnwoundNode>> {
+fn unwind_children(nodes: &Vec<Node>, locals: HashMap<String, Property>, fns: HashMap<String, Function>) -> ParseResult<Vec<UnwoundNode>> {
     let mut unwound_nodes: Vec<UnwoundNode> = Vec::new();
+    let mut locals = locals.clone();
+    let mut fns = fns.clone();
 
     // upsert new local variable scope
     for (ident, value) in extract_variables(&nodes) {
-        locals.insert(ident, value)?;
+        locals.insert(ident, value);
     }
 
     for (ident, value) in collect_function_definitions(&nodes)? {
-        fns.insert(ident, value)?; // note: check for not None as this would override... allowed for now?
+        fns.insert(ident, value); // note: check for not None as this would override... allowed for now?
     }
 
     for node in nodes {
         // if node is a block,
         if let Node::Block{ ident, properties, children } = node {
             // evaluate its children first
-            let unwound_children = unwind_children(&children, &locals, &fns)?;
+            let unwound_children = unwind_children(&children, locals.clone(), fns.clone())?;
 
             // let eval_locals = evaluate_variable_scope(&properties, locals);
-            let eval_result = evaluate_block(ident, properties, locals, &unwound_children, fns)?;
+            let eval_result = evaluate_block(ident, properties, &locals, &unwound_children, &fns)?;
 
             // unwound_children.extend(eval_result.iter().cloned());
             unwound_nodes.extend(eval_result.iter().cloned());
@@ -59,7 +61,7 @@ fn evaluate_block(ident: &String, properties: &Vec<Property>, locals: &HashMap<S
         let args = merge_arguments(&func.arguments, properties)?;
         println!("executing fn: {} {:?} {:?} {:?} {:?} {:?}", ident, func.children, func.arguments, locals, properties, args);
         // need to unwind arguments
-        return unwind_children(&func.children, &args, fns);
+        return unwind_children(&func.children, args, fns.clone());
     }
 
     // println!("fn not found: {}", ident);
